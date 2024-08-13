a3a6f19fbd8617c3a1bb06d2ec60bf96fd9e39d8
William Jordan <the.william.jordan@gmail.com>
2024-08-13 13:29:55 -0400

    server test

--- Geo/src/main.tsx
+++ Geo/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GMap from "./components/GMap.tsx";
+// import GMap from "./components/GMap.tsx";
 import UMap from "./components/UMap.tsx";
-import PinPicker from "./components/PinPicker.tsx";
+// import PinPicker from "./components/PinPicker.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
