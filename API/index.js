fb9c1b7c72e24a97a80f82f9a982c6d7a8f4e8f5
William Jordan <the.william.jordan@gmail.com>
2024-08-16 12:43:45 -0400

    port change

--- API/index.js
+++ API/index.js
@@ -15,10 +15,10 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
-
+  
 
 let items = [];
 
